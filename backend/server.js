4264cdb161b17ceac3bd0ee89d4050a306a8bcf2
elanrbulbul <elanur@netrisk.com.tr>
2025-04-06 17:32:20 +0300

    CreatedJsonFile

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const carparksData = JSON.parse(fs.readFileSync("./carparks.json", "utf8"));
 
-const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    }
-  });
-  
-
-app.get("/carparks", async (req, res) => {
-    try {
-      const carparks = await knex("carparks").select("*");
-      res.json(carparks);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Hata oluştu" });
-    }
-  });
+app.get("/carparks", (req, res) => {
+  res.json(carparksData); 
+});
 app.get("/google-maps-key", (req, res) => {
     res.json({ apiKey: process.env.GOOGLE_MAPS_API_KEY });
 });
